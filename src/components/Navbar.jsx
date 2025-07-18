a6cbabc40ebb766564b1097c3ae44ea59f5fd532
Mithlesh Kumar <mithleshji2021@gmail.com>
2025-07-18 16:07:10 +0530

    logo fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className=' flex justify-between text-[#29969c] bg-[#c6eefc] px-8 py-3 rounded-full mt-2 mx-4 shadow'>
             <div className='flex gap-2 items-center'>
-                <img src="public/frame.png" className='w-6 h-6' alt="QR" />
+                <img src="frame.png" className='w-6 h-6' alt="QR" />
                 <h1 className='text-2xl font-bold '>QR Code Generator</h1>
             </div>
             <h3>...</h3>
